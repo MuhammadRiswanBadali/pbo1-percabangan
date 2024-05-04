2a4c58f48feeb2c9803ef3e87fc3eb3153d8da19
MuhammadRiswanBadali <riswanbadali9@gmail.com>
2024-05-04 13:31:05 +0800

    perbaikan

--- percabanganIF.java
+++ percabanganIF.java
@@ -8,17 +8,23 @@ public class percabanganIF {
         int nilai;
         String nama;
 
-        System.out.println("nama : ");
+        System.out.println("Masukkan Nama : ");
         nama = scan.nextLine();
-        System.out.println("nilai : ");
+        System.out.println("Masukkan Nilai : ");
         nilai = scan.nextInt();
 
-        if(nilai>=80){
-            System.out.println("selamat " + nama + ", anda lulus!!!");
-        } else{
-            System.out.println("sayang sekali " + nama + ", anda tidak lulus");
+        if(nilai>=90 && nilai<=100){
+            System.out.println("Selamat " + nama + ", anda mendapatkan predikat A");
+        } else if(nilai >=80 && nilai<90){
+            System.out.println("Selamat " + nama + ", anda mendapatkan predikat B");
+        } else if(nilai>=70 && nilai<80){
+            System.out.println("Selamat " + nama + ", anda mendapatkan predikat C");
+        } else if(nilai>=60 && nilai <70){
+            System.out.println("Selamat " + nama + ", anda mendapatkan predikat D");
+        } else if(nilai>=0 && nilai<60){
+            System.out.println("Selamat " + nama + ", anda mendapatkan predikat E");
+        } else {
+            System.out.println("Nilai tidak valid!!!");
         }
-
-        
     }
 }
