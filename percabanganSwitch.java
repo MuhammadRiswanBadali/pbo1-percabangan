44c9b11740bfc961996ea81d7c68a8a7b5cc96fd
MuhammadRiswanBadali <riswanbadali9@gmail.com>
2024-05-04 13:47:34 +0800

    percabangan switch

---
+++ percabanganSwitch.java
@@ -0,0 +1,18 @@
+public class percabanganSwitch {
+    
+    public static void main (String[] args){
+
+        int bulan = 1 ;
+
+        switch(bulan){
+            case 1:
+                System.out.println("bulan januari");
+                break;
+            case 2:
+                System.out.println("bulan februari");
+                break;
+            default:
+                System.out.println("bulan tidak ada");
+        }
+    }
+}
