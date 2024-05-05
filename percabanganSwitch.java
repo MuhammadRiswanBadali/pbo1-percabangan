ce142b376e20a78a5738b2fa7c16d8ece8d5e714
MuhammadRiswanBadali <riswanbadali9@gmail.com>
2024-05-05 21:47:00 +0800

    operasi matematika

--- percabanganSwitch.java
+++ percabanganSwitch.java
@@ -1,18 +1,39 @@
+import java.util.Scanner;
+
 public class percabanganSwitch {
-    
-    public static void main (String[] args){
 
-        int bulan = 1 ;
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
 
-        switch(bulan){
-            case 1:
-                System.out.println("bulan januari");
+        String operator;
+        double angka1;
+        double angka2;
+        double hasil =0;
+
+        System.out.println("Masukkan Operator Matematika ( + , - , / , * )");
+        operator = scan.nextLine();
+        System.out.println("Masukkan Angka Pertama : ");
+        angka1 = scan.nextDouble();
+        System.out.println("Masukkan Angka Kedua : ");
+        angka2 = scan.nextDouble();
+
+        switch(operator){
+            case "+":
+                hasil = angka1 + angka2;
                 break;
-            case 2:
-                System.out.println("bulan februari");
+            case "-":
+                hasil = angka1 - angka2;
+                break;
+            case "/":
+                hasil = angka1 / angka2;
+                break;
+            case "*":
+                hasil = angka1 * angka2;
                 break;
             default:
-                System.out.println("bulan tidak ada");
+                System.out.println("Operator yang dimasukkan tidak valid!!!");
+                break;
         }
+        System.out.println("Hasil operasi (" + operator + ") adalah = " + hasil);
     }
 }
